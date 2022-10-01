e78e822cb5ab1d9bcb27dbc6917c2e3d57752fb2
hugmess <240109nb@gmail.com>
2022-10-01 11:53:57 +0300

    темс, забыла что это...

--- pups/character.py
+++ pups/character.py
@@ -1,7 +1,31 @@
+from pups.character2 import Character
+from pups.items import Item
 from enum import Enum, auto
 
 
 class CharacterStats(Enum):
     HP = auto()
     DAMAGE = auto()
-    ARMOR = auto()
+    DEFENCE = auto()
+
+
+class CharacterWithItems(Character):
+    def __init__(self, name="", hp=30, damage=1, defence=0):
+        Character.__init__(self, name, hp, damage)
+        self.defence = defence
+        self.weapon = None
+        self.armor = None
+
+    def set_weapon(self, item: Item):
+        self.weapon = item
+
+    def set_armor(self, item: Item):
+        self.armor = item
+
+    def attack(self, target):
+        try:
+            additional_damage = self.weapon.use()[CharacterStats.DAMAGE]
+        except Exception:
+            additional_damage = 0
+
+        target.take_damage(self.damage + additional_damage)
